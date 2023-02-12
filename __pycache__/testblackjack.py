067a5b62983250d88d97821964c8cd524d5c1232
NickMusy <122288664+NickMusy@users.noreply.github.com>
2023-02-12 18:38:37 -0500

    commit haha

---
+++ __pycache__/testblackjack.py
@@ -0,0 +1,7 @@
+player_hand = 0
+
+for player_hand in range(22):
+	if player_hand >= 21:
+		break
+        
+player_hand += 1
--- blackjack.py
+++ blackjack.py
@@ -2,6 +2,8 @@ import p1_random as p1
 
 def main():
 
+    #defining variables inside main
+
     game_continue = True
     player_hand = 0
     menu = '1.  Get another card\n2.  Hold hand\n3.  Print statistics\n4.  Exit\n'
@@ -10,51 +12,82 @@ def main():
     game_num = 1
     tie_games = 0
     
+    #creating an array that includes the names of the cards and their corresponding values
+
     cards = [['ACE', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'JACK', 'QUEEN', 'KING'],[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]]
     
     rng = p1.P1Random()
 
     print(f'START GAME #{game_num}\n')
 
+    #while loop so that the game will continue for as long as the player wants to play
+
     while game_continue:
+
+        #creates a new value from p1.random and sets it equal to the player's hand
 
         new_card = rng.next_int(13) + 1
 
+        #adds the new card value to the player's hand
+
         player_hand += cards[1][new_card - 1]
 
+        if player_hand == 21:
+            print('Your card is a '+cards[0][new_card - 1]+'!')
+            print(f'Your hand is: {player_hand}\n')
+
+            win(False,False,False,False,True)
+            player_wins += 1
+            player_hand = 0
+            game_num += 1
+            print(f'START GAME #{game_num}\n')
+                    
         print('Your card is a '+cards[0][new_card - 1]+'!')
         print(f'Your hand is: {player_hand}\n')
-        print(menu)
 
-        menu_select = int(input('Choose an option: '))
-        print()
+        if player_hand < 21:
+            print(menu)
+
+        #takes an input from the player of the menu options
+
+            menu_select = int(input('Choose an option: '))
+            print()
+        
+        else:
+            break
+
+        #using a match case statement to more easily format how the program will progress when a menu selection is picked
 
         match menu_select:
 
             case 1:
 
                 if player_hand > 21:
-                    win(False,True,False,False)
+                    win(False,True,False,False,False)
                     dealer_wins += 1
                     player_hand = 0
                     game_num += 1
                     print(f'START GAME #{game_num}\n')
             
+
             case 2:
                 
                 dealer_hand = rng.next_int(11) + 16
+
+                print('Dealer\'s hand:', dealer_hand)
+                print(f'Your hand is: {player_hand}\n')
                 
                 if dealer_hand > 21:
-                    player_wins += win(True,False,False,False)
+                    player_wins += win(True,False,False,False,False)
 
                 elif player_hand > dealer_hand and player_hand <= 21:
-                    player_wins += win(True,False,False,False)
+                    player_wins += win(True,False,False,False,False)
 
                 elif player_hand < dealer_hand and dealer_hand <= 21:
-                    dealer_wins += win(False,False,True,False)
+                    dealer_wins += win(False,False,True,False,False)
 
                 elif player_hand == dealer_hand:
-                    tie_games += win(False,False,False,True)
+                    tie_games += win(False,False,False,True,False)
 
                 player_hand = 0
 
@@ -76,20 +109,26 @@ def main():
             case _:
                 print('Invalid input!\nPlease enter an integer value between 1 and 4.')
 
-def win(player:bool, bust:bool, dealer:bool, tie:bool):
+
+#defining a global win variable in order to make the case arguments less lengthy
+
+def win(player:bool, bust:bool, dealer:bool, tie:bool, blackjack:bool):
     
-    if player == True and bust == False and dealer == False and tie == False:
+    if player == True and bust == False and dealer == False and tie == False and blackjack == False:
         print('You win!\n')
 
-    elif player == False and bust == True and dealer == False and tie == False:
+    elif player == False and bust == True and dealer == False and tie == False and blackjack == False:
         print('You exceeded 21! You lose.\n')
     
-    elif player == False and bust == False and dealer == True and tie == False:
+    elif player == False and bust == False and dealer == True and tie == False and blackjack == False:
         print('Dealer wins!\n')
 
-    elif player == False and bust == False and dealer == False and tie == True:
+    elif player == False and bust == False and dealer == False and tie == True and blackjack == False:
         print('It\'s a tie! No one wins!\n')
     
+    elif player == False and bust == False and dealer == False and tie == False and blackjack == True:
+        print('BLACKJACK! You win!\n')  
+
     return 1
 
 main()
